51c4309e6606aeb82bf18617c049134dcb0b9d93
Ashleigh <c00225954@itcarlow.ie>
2020-11-15 23:18:00 +0000

    Scaling Item Display
    Init Method

--- Shop Component RWM/Assets/Scripts/ShopMenuController.cs
+++ Shop Component RWM/Assets/Scripts/ShopMenuController.cs
@@ -37,13 +37,28 @@ public class ShopMenuController : MonoBehaviour
     public Font font;
     [Range(1, 50)]
     public float fontSize;
-  
+
     public Color fontColor;
-   
+
     private float BGWidth;
     private float BGHeight;
 
-   
+    [Tooltip("The background image for item Window(.PNG)")]
+    public Sprite gridBackgroundSprite;
+    [Tooltip("The border/window for an item being displayed(.PNG)")]
+    public Sprite itemBorderBoxSprite;
+
+    public Vector2 purchaseWindowPos;
+    public Vector2 purchaseGridBackgroundSize;
+
+    [Range(1, 50)]
+    public int windowSlots;
+    [Range(10, 200)]
+    public float cellsize;
+    [Range(1, 10)]
+    public int constraintCount;
+    [Range(1, 100)]
+    public float itemBoxGridOffset;
 
 
     // Start is called before the first frame update
@@ -52,13 +67,14 @@ public class ShopMenuController : MonoBehaviour
 
         InitBackgroundImage();
         InitTitle();
-      
+        
+        InitPanel();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void InitBackgroundImage()
@@ -74,15 +90,15 @@ public class ShopMenuController : MonoBehaviour
         backgroundImage.rectTransform.anchoredPosition = backgroundPosition;
         backgroundImage.rectTransform.sizeDelta = new Vector2(Width, Height);
 
-       
+
     }
 
     void InitTitle()
     {
-        
+
         TitlePosition.y *= -1;
         Text TitleOb = GetComponentInChildren<Text>();
-      
+
         if (font != null)
         {
             TitleOb.font = font;
@@ -100,6 +116,61 @@ public class ShopMenuController : MonoBehaviour
         TitleOb.rectTransform.anchoredPosition = TitlePosition;
     }
 
-   
+  
+    void InitPanel()
+    {
+        GameObject panel = new GameObject();
+        panel.name = "Panel Tile Grid";
+        panel.AddComponent<CanvasRenderer>();
+        panel.AddComponent<RectTransform>();
+        panel.AddComponent<Image>();
+        panel.AddComponent<GridLayoutGroup>();
+        panel.transform.SetParent(transform);
 
+        GridLayoutGroup grid = panel.GetComponent<GridLayoutGroup>();
+        // cell size
+        // offset
+        // type of 
+
+        grid.cellSize = new Vector2(cellsize, cellsize);
+        RectTransform panelRectTransform = panel.GetComponent<RectTransform>();
+        panelRectTransform.anchorMin = new Vector2(0, 1);
+        panelRectTransform.anchorMax = new Vector2(0, 1);
+        panelRectTransform.pivot = new Vector2(0.0f, 1f);
+        panelRectTransform.anchoredPosition = new Vector2(0, 0);
+
+
+        panel.GetComponent<Image>().sprite = gridBackgroundSprite;
+        panel.GetComponent<RectTransform>().sizeDelta = purchaseGridBackgroundSize;
+        purchaseWindowPos.y *= -1;
+        panelRectTransform.anchoredPosition += purchaseWindowPos;
+
+
+        grid.SetLayoutHorizontal();
+        grid.constraint = GridLayoutGroup.Constraint.Flexible;
+        CalculateColumnCount(grid);
+        grid.cellSize = new Vector2(cellsize, cellsize);
+        grid.padding.left = 20;
+        grid.padding.top = 20;
+        grid.padding.right = 20;
+        grid.padding.bottom = 20;
+        for (int i = 0; i < windowSlots; i++)
+        {
+            GameObject windowSlot = new GameObject();
+            windowSlot.AddComponent<Image>();
+            windowSlot.GetComponent<Image>().sprite = itemBorderBoxSprite;
+            windowSlot.transform.SetParent(grid.transform);
+        }
+    }
+
+    void CalculateColumnCount(GridLayoutGroup g)
+    {
+        constraintCount = g.constraintCount;
+        int numInRow = windowSlots / constraintCount;
+        // cellsize = (purchaseGridBackgroundSize.x - 20 - itemBoxGridOffset * ( numInRow )) / numInRow;
+        // float boxy = ((cellsize + itemBoxGridOffset) * numInRow) - itemBoxGridOffset;
+        g.spacing = new Vector2(itemBoxGridOffset, itemBoxGridOffset);
+        // g.constraintCount = constraintCount;
+
+    }
 }
