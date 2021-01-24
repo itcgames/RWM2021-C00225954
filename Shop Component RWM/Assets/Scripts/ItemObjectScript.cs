d8ac58f66fa4ea0e3d56cd981ace67b8fb7e996b
Ashleigh <c00225954@itcarlow.ie>
2021-01-24 15:35:41 +0000

    clean

--- Shop Component RWM/Assets/Scripts/ItemObjectScript.cs
+++ Shop Component RWM/Assets/Scripts/ItemObjectScript.cs
@@ -4,53 +4,15 @@ using UnityEngine;
 
 public class ItemObjectScript : MonoBehaviour
 {
-    public int priceOfItem;
-    public Sprite itemSprite; // if you need to change item sprite
-    public string ItemName;
-    public int coolDownAfterPurchase;
-   
-    public int actionCoolDownTimer;
-    int currentActionCoolDownCount;
-
-    bool itemBought;
-    public int itemID; // mainly used to just scale the item correctly due to varying sprite sizes
-  
-    void Start()
+    private void Start()
     {
-        if (itemSprite != null)
-        {
-            GetComponent<SpriteRenderer>().sprite = itemSprite;
-           
-        }
-     
-
-        itemBought = true;
-
-        currentActionCoolDownCount = actionCoolDownTimer;
-     
+        
     }
 
-    public Sprite GetItemSpriteFromScript()
+    private void Update()
     {
-        if (itemSprite != null)
-        {
-            return itemSprite;
-        }
-        else
-        {
-            return GetComponent<SpriteRenderer>().sprite;
-        }
+        
     }
-
-    public string GetItemName() { return ItemName; }
-
-    public int GetCoolDownTime() { return coolDownAfterPurchase; }
-
-    public void setBought(bool t_isBought) { itemBought = t_isBought; }
-
-    public void setPosition(Vector2 t_pos) { transform.position = t_pos; }
-
-    public int GetPriceOfItem() { return priceOfItem; }
 
 
 }
